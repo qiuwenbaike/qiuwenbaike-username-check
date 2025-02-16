e85ed991b7d7b6fdd189d489148bfc6e68b6195a
WaitSpring <me@waitspring.com>
2025-02-16 18:44:13 +0800

    merge check area

--- index.php
+++ index.php
@@ -57,7 +57,6 @@ EOF;
 
 if ($user !== $info["name"] || isset($info["userid"])) {
 	$pageContent = $pageContent . <<<EOF
-
 		<h3>技术性检查</h3>
 	EOF;
 
@@ -109,7 +108,15 @@ if (isset($info["invalid"])) {
 EOF;
 }
 
-if (isset($info["cancreateerror"])) {
+$pageContent = $pageContent . <<<EOF
+<h3>合规性自动检查</h3>
+<p>以下自动检查旨在确认您的用户名是否违反<a href="https://www.qiuwenbaike.cn/wiki/Qiuwen:用户名" target="_blank">用户名方针</a>。</p>
+EOF;
+
+if (
+	isset($info["cancreateerror"]) ||
+	preg_match("/(求闻|求聞|站长|站長|管理員|行政員|監管員|裁決委員|使用者核查員|使用者查核員|監督員|裁决委员|管理员|行政员|监管员|用户核查员|用户查核员|监督员|机器人|机械人|機器人|機械人|qiuwen|admin|sysop|moderator|bureaucrat|steward|checkuser|oversight|bot$)/i", $info["name"])
+) {
 	$nameCannotCreated = $info["name"];
 	$encodedNameCannotCreated = urlencode($nameCannotCreated);
 	$pageContent = $pageContent . <<<EOF
@@ -118,45 +125,85 @@ if (isset($info["cancreateerror"])) {
 		您的用户名不可建立。
 	</span>
 <p>
+<p>原因：</p>
+<ul>
 EOF;
 
-	$cancreateerror = $info["cancreateerror"][0];
-	$message = $cancreateerror["message"];
+	if (isset($info["cancreateerror"])) {
+		$cancreateerror = $info["cancreateerror"][0];
+		$message = $cancreateerror["message"];
 
-	if ($message == "userexists") {
-		$pageContent = $pageContent . <<<EOF
-		<p>原因：已被他人使用。</p>
-		<p>参见：</p>
-		<ul>
-			<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CentralAuth?target=$encodedNameCannotCreated" target="_blank">全域账号信息</a></li>
-			<li><a href="https://www.qiuwenbaike.cn/wiki/Special:UserRights?user=$encodedNameCannotCreated" target="_blank">权限授予信息</a></li>
-		</ul>
+		if ($message == "userexists") {
+			$pageContent = $pageContent . <<<EOF
+		<li>
+			<p>已被他人使用。</p>
+			<p>参见：</p>
+			<ul>
+				<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CentralAuth?target=$encodedNameCannotCreated" target="_blank">全域账号信息</a></li>
+				<li><a href="https://www.qiuwenbaike.cn/wiki/Special:UserRights?user=$encodedNameCannotCreated" target="_blank">权限授予信息</a></li>
+			</ul>
+		</li>
 		EOF;
-	} else if ($message == "noname") {
-		$pageContent = $pageContent . "<p>原因：不可使用电子邮件地址作为用户名。</p>";
-	} else if ($message == "titleblacklist-forbidden-new-account") {
-		$pageContent = $pageContent . "<p>原因：用户名包含列入黑名单的关键词。</p>";
-	} else if ($message == "antispoof-conflict") {
-		$pageContent = $pageContent . "<p>原因：用户名与其他用户名过于相似，请选择其它用户名。</p>";
-	} else {
-		$pageContent = $pageContent . "<p>原因：用户名因其他技术原因无法创建，请选择其它用户名。</p>" . <<<EOF
-		<p>
-		技术信息（请报告管理员）：
-		<code>$message</code>
-		</p>
+		} else if ($message == "noname") {
+			$pageContent = $pageContent . "<li>不可使用电子邮件地址作为用户名。</li>";
+		} else if ($message == "titleblacklist-forbidden-new-account") {
+			$pageContent = $pageContent . "<li>用户名包含列入黑名单的关键词。</li>";
+		} else if ($message == "antispoof-conflict") {
+			$pageContent = $pageContent . "<li>用户名与其他用户名过于相似，请选择其它用户名。</li>";
+		} else {
+			$pageContent = $pageContent . <<<EOF
+		<li>
+			<p>用户名因其他技术原因无法创建，请选择其它用户名。</p>
+			<p>
+			技术信息（请报告管理员）：
+			<code>$message</code>
+			</p>
+		</li>
 		EOF;
+		}
 	}
 
+	if (preg_match("/(求闻|求聞|站长|站長|管理員|行政員|監管員|裁決委員|使用者核查員|使用者查核員|監督員|裁决委员|管理员|行政员|监管员|用户核查员|用户查核员|监督员|qiuwen|admin|sysop|moderator|bureaucrat|steward|checkuser|oversight)/i", $info["name"], $m)) {
+		$match = $m[1];
+		$pageContent = $pageContent . <<<EOF
+	<li>
+		<span style="color: red;">
+			您的用户名包含了特定字词“$match ”，可能误导他人您的账户拥有特定权限。
+		</span>
+	</li>
+	EOF;
+	}
+
+	if (preg_match("/(机器人|机械人|機器人|機械人|bot$)/i", $info["name"], $m)) {
+		$pageContent = $pageContent . <<<EOF
+	$match = $m[1];
+	<li>
+		<span style="color: red;">
+			您的用户名包含了特定字词“$match ”，可能误导他人您的账户是机器人账户，除非您要创建一个机器人账户。
+		</span>
+	</li>
+	EOF;
+	}
+
+	$pageContent = $pageContent . "</ul>";
 
 	if (isset($_GET["admin"])) {
 		$pageContent = $pageContent . <<<EOF
-		<p>仍要创建？</p>
-		<ul>
-			<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CreateAccount?wpName=$encodedNameCannotCreated" target="_blank">继续创建</a></li>
-			<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CreateAccount?wpName=$encodedNameCannotCreated&wpCreateaccountMail=1"" target="_blank">继续创建<small>（随机密码）</small></a></li>
-		</ul>
-		EOF;
+	<h4>仍要创建？</h4>
+	<ul>
+		<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CreateAccount?wpName=$encodedNameCannotCreated" target="_blank">继续创建</a></li>
+		<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CreateAccount?wpName=$encodedNameCannotCreated&wpCreateaccountMail=1"" target="_blank">继续创建<small>（随机密码）</small></a></li>
+	</ul>
+	EOF;
 	}
+} else {
+	$pageContent = $pageContent . <<<EOF
+	<p>
+		<span style="color: green;">
+			自动检查未发现任何问题。
+		</span>
+	</p>
+	EOF;
 }
 
 if (isset($info["cancreate"])) {
@@ -175,41 +222,6 @@ if (isset($info["cancreate"])) {
 		<li><a href="https://www.qiuwenbaike.cn/wiki/Special:CreateAccount?wpName=$encodedNameCanCreate&wpCreateaccountMail=1"" target="_blank">立即创建<small>（随机密码）</small></a></li>
 	</ul>
 EOF;
-}
-
-
-$pageContent = $pageContent . <<<EOF
-<h3>合规性检查</h3>
-<p>以下检查旨在确认您的用户名是否存在违反<a href="https://www.qiuwenbaike.cn/wiki/Qiuwen:用户名" target="_blank">用户名方针</a>之处。</p>
-EOF;
-
-
-if (preg_match("/(求闻|求聞|站长|站長|管理員|行政員|監管員|裁決委員|使用者核查員|使用者查核員|監督員|裁决委员|管理员|行政员|监管员|用户核查员|用户查核员|监督员|qiuwen|admin|sysop|moderator|bureaucrat|steward|checkuser|oversight)/i", $info["name"], $m)) {
-	$match = $m[1];
-	$pageContent = $pageContent . <<<EOF
-	<p>
-		<span style="color: red;">
-			您的用户名包含了特定字词“$match ”，可能误导他人您的账户拥有特定权限。
-		</span>
-	</p>
-	EOF;
-} else if (preg_match("/(机器人|机械人|機器人|機械人|bot$)/i", $info["name"], $m)) {
-	$pageContent = $pageContent . <<<EOF
-	$match = $m[1];
-	<p>
-		<span style="color: red;">
-			您的用户名包含了特定字词“$match ”，可能误导他人您的账户是机器人账户，除非您要创建一个机器人账户。
-		</span>
-	</p>
-	EOF;
-} else {
-	$pageContent = $pageContent . <<<EOF
-	<p>
-		<span style="color: green;">
-			自动检查未发现任何问题。
-		</span>
-	</p>
-	EOF;
 }
 
 $pageContent = $pageContent . <<<EOF
